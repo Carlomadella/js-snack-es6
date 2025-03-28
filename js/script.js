011f6605bed11973f59a341b53d559e3394b735f
Carlomadella <madella871@gmail.com>
2025-03-28 14:59:35 +0100

    [UPD] Snack 1: creato array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 // Snack 1: Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
 
+let bici = [
+    {
+        nome : "Bianchi",
+        peso : 18,
+    },
+    {
+        nome : "Bottecchia",
+        peso : 25,
+    },
+    {
+        nome : "Cipollini",
+        peso : 12,
+    },
+    {
+        nome : "Faema",
+        peso : 37,
+    },
+    {
+        nome : "Molteni",
+        peso : 22,
+    },
+    {
+        nome : "Bottecchia",
+        peso : 10,
+    },
+]
+
