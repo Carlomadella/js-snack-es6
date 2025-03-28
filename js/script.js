3a7192cef88f50a74cc20a3038d587df0f678c8a
Carlomadella <madella871@gmail.com>
2025-03-28 16:09:46 +0100

    [UPD] Snack 1: create variabili utili

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Snack 1: Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare in console la bici con peso minore.
 
-let bici = [
+const bici = [
     {
         nome : "Bianchi",
         peso : 18,
@@ -27,3 +27,9 @@ let bici = [
     },
 ]
 
+console.log(bici);
+
+// inserisco in una variabile il valore del peso del primo elemento dell'array di oggetti
+let pesoMinore = bici[0].peso;
+// creao una variabile per immagazzinare l'elemento dell'array con il peso minore
+let minWeight;
